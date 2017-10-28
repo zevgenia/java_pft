a1bacbde1da9f83cadc4c60365c1ce429e033a4e
zevgenia <zatonskaya@yandex.ru>
2017-10-28 19:44:44 +0300

    В объекты типа Квадрат и Прямоугольник добавлены методы
    Функции вычисления площади квадрата и прямоугольник переделаны в методы,
    ассоциированные с соответствующими объектами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,21 +8,11 @@ public class MyFirstProgram {
     System.out.println("Hello, world!");
 
     Square s = new Square(5); // создание объекта типа Квадрат и передача конкретного значения в конструктор для заполенния
-    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s)); //в качестве пераметра передается созданный объект
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area()); //обращение к методу объекта Квадрат
 
 
-    Rectangle r = new Rectangle(); //создание объекта типа Прямоугольник
-    r.a = 4; //заполнение атрибутов объекта конкретными значениями
-    r.b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
-  }
-
-  public static double area(Square s) { //функция в качестве параметра принимает объект типа Квадрат
-    return s.l * s.l; //доступ к атрибутам объекта
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
+    Rectangle r = new Rectangle(4,6); //создание объекта типа Прямоугольник
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area()); //вызов метода объекта Прямоугольник
   }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -5,4 +5,14 @@ public class Rectangle {
 //прямоугольник описывается длинами двух его сторон
   public double a;
   public double b;
+
+  public Rectangle (double a, double b){//это констуктор
+
+    this.a = a;
+    this.b = b;
+  }
+
+  public double area() {
+    return this.a * this.b;
+  }
 }
