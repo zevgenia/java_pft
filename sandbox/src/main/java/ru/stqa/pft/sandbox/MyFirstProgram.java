f29edb6c66337f201185abc6f09678e1852500ef
zevgenia <zatonskaya@yandex.ru>
2017-10-28 19:01:55 +0300

    Классы, объекты и конструкторы объектов.
    Два способа заполнения атрибутов объектов конкретными значениями - с
    помощью присваивания перед вызовом функции,в которую передается объект,
    либо с помощью конструктора в самом классе, описывающем объект.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	System.out.println("Hello, world!");
-    }
+    System.out.println("Hello, world!");
+
+    Square s = new Square(5); // создание объекта типа Квадрат и передача конкретного значения в конструктор для заполенния
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s)); //в качестве пераметра передается созданный объект
+
+
+    Rectangle r = new Rectangle(); //создание объекта типа Прямоугольник
+    r.a = 4; //заполнение атрибутов объекта конкретными значениями
+    r.b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+  }
+
+  public static double area(Square s) { //функция в качестве параметра принимает объект типа Квадрат
+    return s.l * s.l; //доступ к атрибутам объекта
+  }
+
+  public static double area(Rectangle r) {
+    return r.a * r.b;
+  }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,8 @@
+package ru.stqa.pft.sandbox;
+
+public class Rectangle {
+
+//прямоугольник описывается длинами двух его сторон
+  public double a;
+  public double b;
+}
